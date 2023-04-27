cb3bc0dcea921afbaeb2991332f71f41b8b570fc
diawando <abduldiawade777@gmail.com>
2023-04-27 15:42:13 +0000

    ajout des fonctionalités de gestion des tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ jQuery('#datedefin').datetimepicker({
 
 });
 
-
+// filtre les voitures 
  $('.tri-wrapper .btn-group button').click(function () {
     $(this).addClass('active').siblings().removeClass('active');
     
@@ -24,6 +24,50 @@ jQuery('#datedefin').datetimepicker({
     $('.grid').children().hide();
     filteredChildren.show();
     $('.num-filtered-children').text(numFilteredChildren);
+  });
+
+   // tab activation code
+
+  // Initialize active tab index variable
+  let activeTabIndex = 1;
+
+  // Add click event listeners to the next and previous links
+  const nextLinks = document.querySelectorAll('.next-tab');
+  const prevLinks = document.querySelectorAll('.prev-tab');
+
+  nextLinks.forEach((link) => {
+      link.addEventListener('click', (event) => {
+          const activeTab = document.querySelector('.tab-pane.active');
+          const nextTab = document.querySelector(event.target.hash);
+          const nextTabLink = document.querySelector(`a[href="${event.target.hash}"]`);
+          const prevTabIndex = activeTabIndex;
+
+          activeTab.classList.remove('show', 'active');
+          nextTab.classList.add('show', 'active');
+          nextTabLink.classList.add('active');
+          if (prevTabIndex !== 0) {
+              const prevTabLink = document.querySelectorAll('.nav-tabs .nav-link')[prevTabIndex - 1];
+              prevTabLink.classList.add('completed');
+          }
+          activeTabIndex++;
+      });
+  });
+
+  prevLinks.forEach((link) => {
+      link.addEventListener('click', (event) => {
+          const activeTab = document.querySelector('.tab-pane.active');
+          const prevTab = document.querySelector(event.target.hash);
+          const prevTabLink = document.querySelector(`a[href="${event.target.hash}"]`);
+
+          activeTab.classList.remove('show', 'active');
+          prevTab.classList.add('show', 'active');
+          prevTabLink.classList.add('active');
+          if (activeTabIndex !== 0) {
+              const prevTabLink = document.querySelectorAll('.nav-link')[activeTabIndex - 1];
+              prevTabLink.classList.remove('completed');
+          }
+          activeTabIndex--;
+      });
   });
 
 });
